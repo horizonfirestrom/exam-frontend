d86f052fd90a186655ef6ec6f8b837eda171fb55
MD Imran <horizonfirestrom383@gmail.com>
2021-07-06 08:54:50 +0545

    updated

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginData =
+  {
+    username:'',
+    password:'',
+  }
+  constructor(private snack:MatSnackBar, private login:LoginService) { }
 
   ngOnInit(): void {
+  }
+  formSubmit(){
+    console.log('login btn clicked');
+    if(this.loginData.username.trim() == ''  || this.loginData.username == null){
+      this.snack.open("Username is required !!!",'',{
+        duration:3000,
+      });
+      return;
+    }
+
+    if(this.loginData.password.trim() == ''  || this.loginData.password == null){
+      this.snack.open("Password is required !!!",'',{
+        duration:3000,
+      });
+      return;
+    }
+
+    //request to server to generate token
+
+    this.login.generateToken(this.loginData).subscribe(
+      (data:any) => {
+        console.log(data);
+
+
+        //Login ..
+        this.login.loginUser(data.token);
+
+        this.login.getCurrentUser().subscribe(
+          (user:any) => {
+            this.login.setUser(user);
+            console.log(user);
+
+            //redirect to admin if the user is admin or to normal if user is normal
+            if(this.login.getUserRole()=="ADMIN"){
+              //admin dashboard
+              window.location.href='/admin';
+
+            }else if(this.login.getUserRole() == "NORMAL"){
+              //normal user dashboard
+              window.location.href = '/user-dashboard';
+            } else {
+              this.login.logout();
+              
+            }
+          }
+        )
+
+
+      },
+      (error)=>{
+        console.log("Error!!");
+        console.log(error);
+        this.snack.open("Invalid Details !! Try again",'',{
+          duration: 3000,
+        });
+        
+      }
+      );
+
   }
 
 }
--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,2 +1,2 @@
-let baseUrl ="http://localhost:8080"
+let baseUrl ="http://localhost:8282"
 export default baseUrl;
